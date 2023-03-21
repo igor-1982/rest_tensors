57e380a9df52ada4753ac6109c6a338604935c88
Igor Ying Zhang <igor_zhangying@fudan.edu.cn>
2023-03-21 22:05:48 +0800

    IYZ

--- docs.sh
+++ docs.sh
@@ -1,6 +1,6 @@
 #/usr/bin/env bash
 cargo doc --no-deps
 rm -rf ./docs
-echo "<meta http-equiv=\"refresh\" content=\"0; url=rest_tensors\">" > target/doc/index.html
-cp -r target/doc ./docs
+echo "<meta http-equiv=\"refresh\" content=\"0; url=rest_tensors\">" > ../target/doc/index.html
+cp -r ../target/doc ./docs
 
